8ebf49cdc04ad7aa6e382061ac772f6012785642
Fnuworsu <nuworsufelix49@gmail.com>
2025-03-22 15:15:20 -0500

    limiting number of processes

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"strconv"
 	"syscall"
 )
 
@@ -29,9 +31,11 @@ func run() {
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
-	// Adding namespaces
+	// Adding namespaces to kernel
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID,
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+		// not sharing new namespace with host
+		Unshareflags: syscall.CLONE_NEWNS,
 	}
 
 	cmd.Run()
@@ -40,6 +44,7 @@ func run() {
 func child() {
 	fmt.Printf("Running %v as %d\n", os.Args[2:], os.Getpid())
 
+	cg()
 	// Setting up hostname
 	syscall.Sethostname([]byte("container"))
 
@@ -60,6 +65,22 @@ func child() {
 	cmd.Run()
 
 	syscall.Unmount("/proc", 0)
+}
+
+// limiting the number of (caping memory)
+func cg() {
+	cgroups := "/sys/fs/cgroup"
+	pids := filepath.Join(cgroups, "pids")
+	err := os.Mkdir(filepath.Join(pids, "felix"), 0755)
+	if err != nil && !os.IsExist(err) {
+		panic(err)
+	}
+	// Limit control group to 20 processes
+	must(os.WriteFile(filepath.Join(pids, "felix/pids.max"), []byte("20"), 0700))
+	// Removes the new cgroup in place after the container exits
+	must(os.WriteFile(filepath.Join(pids, "felix/notify_on_release"), []byte("1"), 0700))
+	// Write the pid of the current process to the cgroup's cgroup.procs file
+	must(os.WriteFile(filepath.Join(pids, "felix/cgroup.procs"), []byte(strconv.Itoa(os.Getpid())), 0700))
 }
 
 func must(err error) {
