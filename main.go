26a6ad81d273592eeae98abba9c3bf88fcaeef96
Fnuworsu <nuworsufelix49@gmail.com>
2025-03-21 22:57:46 -0500

    set up child process & hostname

--- main.go
+++ main.go
@@ -14,7 +14,8 @@ func main() {
 	switch os.Args[1] {
 	case "run":
 		run()
-
+	case "child":
+		child()
 	default:
 		panic("bad command")
 	}
@@ -23,7 +24,7 @@ func main() {
 func run() {
 	fmt.Printf("Running %v\n", os.Args[2:])
 
-	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
@@ -32,6 +33,21 @@ func run() {
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		Cloneflags: syscall.CLONE_NEWUTS,
 	}
+
+	cmd.Run()
+}
+
+func child() {
+	fmt.Printf("Running %v\n", os.Args[2:])
+
+	// Setting up hostname
+	syscall.sethostname([]byte("container"))
+
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
 	cmd.Run()
 }
 
